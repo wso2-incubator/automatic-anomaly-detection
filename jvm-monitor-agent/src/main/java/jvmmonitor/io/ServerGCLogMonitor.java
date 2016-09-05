fab55f5d4c7145d95638dfe195ec94c59aec51d6
Buddhi Rathnayaka <buddhiayesha.13@cse.mrt.ac.lk>
2016-09-05 16:57:27 +0530

    Add method for test GC log

--- jvm-monitor-agent/src/main/java/jvmmonitor/io/ServerGCLogMonitor.java
+++ jvm-monitor-agent/src/main/java/jvmmonitor/io/ServerGCLogMonitor.java
@@ -18,6 +18,32 @@ package jvmmonitor.io;
 * under the License.
 */
 
+import communicator.DASPublisher;
+import org.wso2.carbon.databridge.agent.exception.DataEndpointAgentConfigurationException;
+import org.wso2.carbon.databridge.agent.exception.DataEndpointAuthenticationException;
+import org.wso2.carbon.databridge.agent.exception.DataEndpointConfigurationException;
+import org.wso2.carbon.databridge.agent.exception.DataEndpointException;
+import org.wso2.carbon.databridge.commons.exception.TransportException;
+
+import java.io.FileNotFoundException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class ServerGCLogMonitor {
+
     private static final String SAMPLE_LOG_PATH = System.getProperty("user.dir") + "/gc.log.2.current";
+
+    public static void main(String[] args) throws DataEndpointException,
+            SocketException,
+            UnknownHostException,
+            FileNotFoundException,
+            DataEndpointConfigurationException,
+            DataEndpointAuthenticationException,
+            DataEndpointAgentConfigurationException,
+            TransportException {
+
+        DASPublisher dasPublisherObj = new DASPublisher();
+        dasPublisherObj.publishXXgcLogData(SAMPLE_LOG_PATH);
+
+    }
 }
