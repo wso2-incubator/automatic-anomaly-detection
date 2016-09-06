0c71a23a78be3affc26dacec76a25edd39559f9d
nadheesh <nadheeshj@gmail.com>
2016-09-06 11:18:14 +0530

    added test directory

---
+++ jvm-monitor-agent/test/java/jvmmonitor/Agent.java
@@ -0,0 +1,94 @@
+package jvmmonitor;/*
+*  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
+import com.sun.tools.attach.VirtualMachineDescriptor;
+import jvmmonitor.exceptions.MonitoringNotStartedException;
+import jvmmonitor.management.MemoryUsageMonitor;
+import jvmmonitor.model.GarbageCollectionLog;
+import org.apache.log4j.Logger;
+
+import javax.management.MalformedObjectNameException;
+import java.io.IOException;
+import java.lang.management.GarbageCollectorMXBean;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Agent {
+
+    final static Logger logger = Logger.getLogger(Agent.class);
+
+    static String pid = "19949";
+
+    //private static MemoryMonitor memoryMonitor = new MemoryMonitor();
+
+    public static void main(String[] args) throws InterruptedException, MonitoringNotStartedException {
+
+
+        logger.info("Currently running");
+        for (VirtualMachineDescriptor vmd : VirtualMachine.list())
+            logger.info(vmd.id() + "\t" + vmd.displayName());
+
+        try {
+            UsageMonitor usageMonitor = new UsageMonitor(pid);
+            usageMonitor.stratMonitoring();
+
+            LinkedList<GarbageCollectionLog> gm = (LinkedList<GarbageCollectionLog>) usageMonitor.getGarbageCollectionMonitor().getGCUsages();
+
+
+            while (true) {
+
+                    if (gm.size() > 0 ){
+                        Date date = new Date(gm.poll().getStartTime());
+                        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy h:mm:ss a");
+                        String formattedDate = sdf.format(date);
+                        System.out.println("Start time : " + formattedDate);
+
+                    }
+                Date date1 = new Date(usageMonitor.getUsageLog().getDate());
+                SimpleDateFormat sdf1 = new SimpleDateFormat("MM/dd/yyyy h:mm:ss a");
+                String formattedDate1 = sdf1.format(date1);
+                System.out.println("Usage Received Time : " + formattedDate1);
+
+//                Map<String,Long> mem_usage = (Map<String,Long>) usageMonitor.getUsageLog().get(UsageMonitor.MEMORY_USAGE_LOG);
+//                logger.info(MemoryUsageMonitor.ALLOCATED_HEAP_MEMORY.concat(" : ").concat(String.valueOf(mem_usage.get(MemoryUsageMonitor.ALLOCATED_HEAP_MEMORY))));
+//                logger.info(MemoryUsageMonitor.USED_HEAP_MEMORY.concat(" : ").concat(String.valueOf(mem_usage.get(MemoryUsageMonitor.USED_HEAP_MEMORY))));
+//                logger.info(MemoryUsageMonitor.MAX_HEAP_MEMORY.concat(" : ").concat(String.valueOf(mem_usage.get(MemoryUsageMonitor.MAX_HEAP_MEMORY))));
+//                logger.info(MemoryUsageMonitor.PENDING_FINALIZATIONS.concat(" : ").concat(String.valueOf(mem_usage.get(MemoryUsageMonitor.PENDING_FINALIZATIONS))));
+//
+                Thread.sleep(1000);
+//
+//                logger.info("");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (AttachNotSupportedException e) {
+            e.printStackTrace();
+        } catch (MalformedObjectNameException e) {
+            e.printStackTrace();
+//        } catch (MonitoringNotStartedException e) {
+//            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ jvm-monitor-agent/test/java/jvmmonitor/management/ThreadPoolLog.java
@@ -0,0 +1,58 @@
+package jvmmonitor.management;
+
+import com.sun.management.OperatingSystemMXBean;
+import com.sun.management.ThreadMXBean;
+import com.sun.tools.attach.AgentInitializationException;
+import com.sun.tools.attach.AgentLoadException;
+import com.sun.tools.attach.VirtualMachine;
+import com.sun.tools.attach.VirtualMachineDescriptor;
+import jvmmonitor.server.Connection;
+
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+
+import static java.lang.management.ManagementFactory.THREAD_MXBEAN_NAME;
+import static java.lang.management.ManagementFactory.newPlatformMXBeanProxy;
+
+/*
+*  Copyright (c) ${date}, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+public class ThreadPoolLog {
+
+
+    static String pid = "7565";
+    public static void main(String[] args) throws InterruptedException, AgentLoadException, IOException, AgentInitializationException {
+
+        System.out.println("Currently running");
+        for (VirtualMachineDescriptor vmd : VirtualMachine.list())
+            System.out.println(vmd.id() + "\t" + vmd.displayName());
+
+
+        Connection connection = Connection.getConnection(pid);
+
+       ThreadMXBean bean = (com.sun.management.ThreadMXBean) ManagementFactory.getThreadMXBean();
+        bean = newPlatformMXBeanProxy(connection.getServerConnection(), THREAD_MXBEAN_NAME, ThreadMXBean.class);
+        while (true) {
+
+            System.out.println( "Thread Count: " + bean.getThreadCount());
+            System.out.println( "Thread CPU time: " + bean.getCurrentThreadCpuTime());
+
+            Thread.sleep(1000);
+        }
+
+    }
+}
