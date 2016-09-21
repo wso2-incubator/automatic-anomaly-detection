55fdb2bb9ad4868ac340af051aeef85b04723bce
Buddhi Rathnayaka <buddhiayesha.13@cse.mrt.ac.lk>
2016-09-21 16:26:54 +0530

    Set argument to pass program parameters, Added new methods

--- jvm-monitor-agent/src/main/java/controller/Test.java
+++ jvm-monitor-agent/src/main/java/controller/Test.java
@@ -34,6 +34,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.UndeclaredThrowableException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This class runs "BadCode.jar" file to get jvm usage data
@@ -43,129 +46,314 @@ public class Test {
 
     final static Logger logger = Logger.getLogger(Test.class);
 
-    public static void main(String[] args) throws IOException,
-            AttachNotSupportedException,
-            MalformedObjectNameException,
-            InterruptedException,
-            MonitoringNotStartedException,
-            DataEndpointAuthenticationException,
-            DataEndpointAgentConfigurationException,
-            TransportException,
-            DataEndpointException,
-            DataEndpointConfigurationException {
+    //If you want to change Default values; please set java file name , file located path relative to project directory & input arguments.
+    private String fileName = "NormalApp3";
+    private String fileExtension = ".java";
+    private String jarFileRelativePath = "/jvm-monitor-agent/src/samples/applications/NormalApp3";
+    private String arg = "";
 
+    private String currentDir = System.getProperty("user.dir");
+    private String jarFilePath = currentDir + jarFileRelativePath + "/";
+    private String appName;
+    private boolean isAbsolutePath = false;
+    private boolean isJarFile = false;
+    private boolean doCompile = true;
+    private boolean doRecursion = false;
+    private boolean killMultipleProcess = true;
 
-        //If you use java code
-        //Set java file name & file located path relative to project directory
-        String fileName = "Executor";
-        String jarFileRelativePath = "/jvm-monitor-agent/src/samples/applications" + "/NormalApp3";
-        String arg = "1 10000";
+    /**
+     * set user argument
+     *
+     * @param args
+     */
+    private void setAegument(String args[]) {
 
-        String currentDir = System.getProperty("user.dir");
-        String jarFilePath = currentDir + jarFileRelativePath + "/";
-        String error = null, out = "";
-        boolean isCompile = true;
-        boolean doCompile = true;
+        List<String> val = setOptions(args);
 
+        if (val.size() >= 2) {
 
-        if ((new File(jarFilePath + fileName + ".class").isFile()) && !doCompile) {
+            fileName = val.get(0);
 
-            String cmd = "java -classpath " + jarFilePath + " " + fileName + " " + arg;
-            logger.info(cmd);
-            try {
-                Runtime.getRuntime().exec(cmd);
-            } catch (Exception e) {
-                logger.error(e);
+            if (isJarFile) {
+                fileExtension = ".jar";
             }
 
-        } else if ((new File(jarFilePath + fileName + ".java").isFile())) {
+            String temPath = val.get(1);
+            if ('/' != temPath.charAt(temPath.length() - 1)) {
+                temPath = temPath + "/";
+            }
+            if ('/' != temPath.charAt(0)) {
+                temPath = "/" + temPath;
+            }
 
-            String cmd = "javac " + jarFilePath + fileName + ".java";
-            logger.info(cmd);
 
-            try {
-                Process prc = Runtime.getRuntime().exec(cmd);
-                BufferedReader stdError = new BufferedReader(new InputStreamReader(prc.getErrorStream()));
-                while ((error = stdError.readLine()) != null) {
-                    out += error + '\n';
-                    isCompile = false;
+            if (isAbsolutePath) {
+                jarFilePath = temPath;
+            } else {
+                jarFilePath = currentDir + temPath;
+            }
+
+            if (val.size() > 2) {
+                for (int x = 2; x < val.size(); x++) {
+                    arg += val.get(x) + " ";
                 }
+                arg = arg.trim();
+            }
 
-                if (!isCompile) {
-                    logger.error(out);
-                    System.exit(0);
+        }
+
+    }
+
+    /**
+     * set Options argument
+     *
+     * @param OptionsArgs
+     * @return
+     */
+    private List setOptions(String OptionsArgs[]) {
+
+        List<String> args = new ArrayList<String>();
+
+        for (int x = 0; x < OptionsArgs.length; x++) {
+
+            String s = OptionsArgs[x];
+            if ("-j".equals(s)) {
+                isJarFile = true;
+            } else if ("-a".equals(s)) {
+                isAbsolutePath = true;
+            } else if ("-f".equals(s)) {
+                doCompile = false;
+            } else if ("-r".equals(s)) {
+                doRecursion = true;
+            } else if ("-x".equals(s)) {
+                killMultipleProcess = false;
+            } else {
+                args.add(s);
+            }
+
+        }
+
+        return args;
+
+    }
+
+    /**
+     * This will kill multiple process if user give "-k" as a options
+     *
+     * @param appName
+     */
+    private void killMultipleProcess(String appName) {
+
+        if (killMultipleProcess) {
+
+            for (VirtualMachineDescriptor vmd : VirtualMachine.list()) {
+                if (vmd.displayName().indexOf(appName) != -1) {
+
+                    try {
+                        killProcess(vmd.id());
+                        logger.info("kill PID : " + vmd.id() + " " + vmd.displayName());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
                 }
+            }
 
-                Thread.sleep(3000);
-                cmd = "java -classpath " + jarFilePath + " " + fileName + " " + arg;
+        }
+    }
+
+    private void killProcess(String pid) throws IOException {
+        Runtime.getRuntime().exec("kill -9 " + pid);
+    }
+
+    private void setAppName() {
+
+        appName = fileName;
+        if (!"".equals(arg)) {
+            appName += " " + arg;
+        }
+    }
+
+    /**
+     * Start monitored application
+     */
+    private void runMonitoredApp() {
+
+        killMultipleProcess(appName);
+
+        String error, out = "";
+        boolean isCompile = true;
+
+        if (!isJarFile) {
+            if ((new File(jarFilePath + fileName + ".class").isFile()) && !doCompile) {
+
+                String cmd = "java -classpath " + jarFilePath + " " + fileName + " " + arg;
                 logger.info(cmd);
-                Runtime.getRuntime().exec(cmd);
-            } catch (Exception e) {
-                logger.error(e);
+                try {
+                    Runtime.getRuntime().exec(cmd);
+                } catch (Exception e) {
+                    logger.error(e);
+                }
+
+            } else if ((new File(jarFilePath + fileName + fileExtension).isFile())) {
+
+                String cmd = "javac " + jarFilePath + fileName + fileExtension;
+                logger.info(cmd);
+
+                try {
+                    Process prc = Runtime.getRuntime().exec(cmd);
+                    BufferedReader stdError = new BufferedReader(new InputStreamReader(prc.getErrorStream()));
+                    while ((error = stdError.readLine()) != null) {
+                        out += error + '\n';
+                        isCompile = false;
+                    }
+
+                    if (!isCompile) {
+                        logger.error(out);
+                        System.exit(0);
+                    }
+
+                    Thread.sleep(3000);
+                    cmd = "java -classpath " + jarFilePath + " " + fileName + " " + arg;
+                    logger.info(cmd);
+                    Runtime.getRuntime().exec(cmd);
+                } catch (Exception e) {
+                    logger.error(e);
+                }
+
+            } else {
+                logger.error("Could not find \"" + fileName + "\" java file in given directory: " + jarFilePath);
+                System.exit(0);
             }
 
         } else {
-            logger.error("Could not find \"" + fileName + "\" java file in given directory: " + jarFilePath);
-            System.exit(0);
-        }
 
-        /*
-        //If you use .jar file pleases uncomment following part and comment above part
-        //Set jar file name & file located path relative to project directory
-        String fileName = "BadCode.jar";
-        String jarFilePath = "/jvm-monitor-agent/src/samples";
+            if (!(new File(jarFilePath + fileName + fileExtension).isFile())) {
+                System.err.println("Could not find .jar file in given directory: " + jarFilePath + fileName + fileExtension);
+                System.exit(0);
+            }
 
-        String currentDir = System.getProperty("user.dir");
-        jarFilePath = currentDir + jarFilePath + "/" + fileName;
+            try {
+                Runtime.getRuntime().exec("java -jar " + jarFilePath + fileName + fileExtension);
+            } catch (Exception e) {
+                logger.error(e);
+            }
 
-        if (!(new File(jarFilePath).isFile())) {
-            System.err.println("Could not find .jar file in given directory: " + jarFilePath);
-            System.exit(0);
         }
 
         try {
-            Runtime.getRuntime().exec("java -jar " + jarFilePath);
-        } catch (Exception e) {
-            logger.error(e);
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        */
+
+    }
+
+    private String getPID() {
 
         String pid = null;
-        int counter = 0;
-        //Assigning "true" for killMultipleProcess variable is not recommended. This will kill all the processes which have given "fileName".
-        boolean killMultipleProcess = true;
 
+        System.err.println("A:" + appName + ":");
         for (VirtualMachineDescriptor vmd : VirtualMachine.list()) {
-            if (vmd.displayName().indexOf(fileName) != -1) {
-                if (counter == 0) {
-                    pid = vmd.id();
-                    logger.info("PID found. PID: " + vmd.id() + "\tName: " + vmd.displayName());
-                } else if (killMultipleProcess) {
-                    logger.info("kill PID : " + vmd.id() + " " + vmd.displayName());
-                    Runtime.getRuntime().exec("kill -9 " + vmd.id());
-                    counter--;
-                }
-                counter++;
+            System.err.println("JVM:" + vmd.displayName());
+            if (vmd.displayName().indexOf(appName) != -1) {
+                pid = vmd.id();
+                logger.info("PID found. PID: " + vmd.id() + "\tName: " + vmd.displayName());
             }
         }
 
+        return pid;
+    }
+
+    private void startSendMonitoredData() throws MalformedObjectNameException,
+            InterruptedException,
+            IOException,
+            AttachNotSupportedException,
+            MonitoringNotStartedException,
+            DataEndpointException,
+            DataEndpointAuthenticationException,
+            DataEndpointAgentConfigurationException,
+            TransportException,
+            DataEndpointConfigurationException {
+
+        runMonitoredApp();
+        String pid = getPID();
+
         if (pid == null) {
             logger.error("Given \"" + fileName + "\" file is not running");
-        } else if (counter == 1) {
+        } else {
 
-            String appID = (jarFileRelativePath.substring(jarFileRelativePath.lastIndexOf('/') + 1).replace('/', '_') + '-' + fileName).trim();
+            String appID = (jarFilePath.substring(jarFilePath.lastIndexOf('/') + 1).replace('/', '_') + '-' + fileName).trim();
 
             Controller controllerObj = new Controller();
             controllerObj.sendUsageData(pid, appID, controllerObj);
 
             try {
-                Runtime.getRuntime().exec("kill -9 " + pid);
+                killProcess(pid);
             } catch (Exception e) {
                 logger.error(e);
             }
-        } else {
-            logger.error("You have multiple \"" + fileName + "\" Process");
         }
 
     }
+
+    /**
+     * This function will be recursion if user give "-r" as a option.
+     */
+    private void runMonitor() {
+
+        try {
+            startSendMonitoredData();
+        } catch (UndeclaredThrowableException e) {
+
+            if (doRecursion) {
+                System.err.println("Monitored process stopped. monitoring app is now restarted");
+                runMonitor();
+            } else {
+                System.err.println("Monitored process stopped. monitoring application is terminated");
+                System.exit(0);
+            }
+
+        } catch (TransportException e) {
+            e.printStackTrace();
+        } catch (DataEndpointConfigurationException e) {
+            e.printStackTrace();
+        } catch (AttachNotSupportedException e) {
+            e.printStackTrace();
+        } catch (MalformedObjectNameException e) {
+            e.printStackTrace();
+        } catch (MonitoringNotStartedException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (DataEndpointException e) {
+            e.printStackTrace();
+        } catch (DataEndpointAgentConfigurationException e) {
+            e.printStackTrace();
+        } catch (DataEndpointAuthenticationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Test testObj = new Test();
+
+        try {
+            if (args.length >= 2) {
+                testObj.setAegument(args);
+            }
+        } catch (Exception e) {
+            logger.error(e);
+        }
+
+        testObj.setAppName();
+        testObj.runMonitor();
+
+    }
+
+
 }
