db63ed773ac7156733aa71577b4ed9820b86e3a1
Buddhi Rathnayaka <buddhiayesha.13@cse.mrt.ac.lk>
2016-12-01 12:26:20 +0530

    Adding data-agent-configurations and getLocalAddress()

--- jvm-monitor-agent/src/main/java/communicator/DASConfigurations.java
+++ jvm-monitor-agent/src/main/java/communicator/DASConfigurations.java
@@ -19,30 +19,95 @@
 
 package communicator;
 
+import org.apache.log4j.Logger;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+/**
+ *
+ */
 public class DASConfigurations {
+
+    private final static Logger logger = Logger.getLogger(DASConfigurations.class);
 
     private final String host;
     private final int defaultThriftPort;
     private final int securePort;
     private final String username;
     private final String password;
+    private final String dataAgentConfPath;
+    private final String trustStorePath;
+    private final String trustStorePassword;
 
 
-    public DASConfigurations(String host, int defaultThriftPort, String username, String password) {
+    public DASConfigurations(String host, int defaultThriftPort, String username, String password, String resourcesFilePath, String trustStorePassword) {
+
+        if (host == "localhost") {
+            try {
+                host = getLocalAddress();
+            } catch (SocketException | UnknownHostException e) {
+                e.printStackTrace();
+            }
+        }
+
         this.host = host;
         this.defaultThriftPort = defaultThriftPort;
         this.securePort = defaultThriftPort + 100;
         this.username = username;
         this.password = password;
+        this.dataAgentConfPath = resourcesFilePath;
+        this.trustStorePath = resourcesFilePath;
+        this.trustStorePassword = trustStorePassword;
+
     }
 
-    public DASConfigurations(String host, int defaultThriftPort, int securePort, String username, String password) {
+    public DASConfigurations(String host, int defaultThriftPort, int securePort, String username, String password, String resourcesFilePath, String trustStorePassword) {
+
+        if (host == "localhost") {
+            try {
+                host = getLocalAddress();
+            } catch (SocketException | UnknownHostException e) {
+                logger.error(e.getMessage(), e);
+            }
+        }
+
         this.host = host;
         this.defaultThriftPort = defaultThriftPort;
         this.securePort = securePort;
         this.username = username;
         this.password = password;
+        this.dataAgentConfPath = resourcesFilePath;
+        this.trustStorePath = resourcesFilePath;
+        this.trustStorePassword = trustStorePassword;
+
     }
+
+    public DASConfigurations(String host, int defaultThriftPort, int securePort, String username, String password, String dataAgentConfPath, String trustStorePath, String trustStorePassword) {
+
+        if (host == "localhost") {
+            try {
+                host = getLocalAddress();
+            } catch (SocketException | UnknownHostException e) {
+                logger.error(e.getMessage(), e);
+            }
+        }
+
+        this.host = host;
+        this.defaultThriftPort = defaultThriftPort;
+        this.securePort = securePort;
+        this.username = username;
+        this.password = password;
+        this.dataAgentConfPath = dataAgentConfPath;
+        this.trustStorePath = trustStorePath;
+        this.trustStorePassword = trustStorePassword;
+
+    }
+
 
     public String getHost() {
         return host;
@@ -62,6 +127,34 @@ public class DASConfigurations {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getDataAgentConfPath() {
+        return dataAgentConfPath;
+    }
+
+    public String getTrustStorePath() {
+        return trustStorePath;
+    }
+
+    public String getTrustStorePassword() {
+        return trustStorePassword;
+    }
+
+    private String getLocalAddress() throws SocketException, UnknownHostException {
+        Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces();
+        while (ifaces.hasMoreElements()) {
+            NetworkInterface iface = ifaces.nextElement();
+            Enumeration<InetAddress> addresses = iface.getInetAddresses();
+
+            while (addresses.hasMoreElements()) {
+                InetAddress addr = addresses.nextElement();
+                if (addr instanceof Inet4Address && !addr.isLoopbackAddress()) {
+                    return addr.toString().replace("/", "");
+                }
+            }
+        }
+        return InetAddress.getLocalHost().toString().replace("/", "");
     }
 
 }
